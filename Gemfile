4552f4de87378b9a1b5bfb6e92e1c168b9f99bbc
Jennifer Pazos <jmp521@gmail.com>
2020-11-03 17:24:14 -0600

    first commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in tv-info.gemspec
+gemspec
+
+gem "rake", "~> 12.0"
---
+++ lib/tv/info.rb
@@ -0,0 +1,8 @@
+require "tv/info/version"
+
+module Tv
+  module Info
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
